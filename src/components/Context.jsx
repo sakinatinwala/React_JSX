e55b6c54f06ed7edf29139645810d9439760b906
Sakina Tinwala <tinwalasakina515253@gmail.com>
2022-09-15 14:10:46 +0530

    - React JSX Form Commited..

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,25 +1,33 @@
- import React, { useState, createContext, useContext } from "react";
+import React, { createContext, useContext } from "react";
+import { SignUp } from "./Register";
+//import { Login } from "./LogIn";
+import { useEffect } from "react";
  
- export const UserContext = createContext(null);
+ export const UserContext = createContext();
+
+ export const Component1 = (props) => {
+    const [data, setData] = useContext(SignUp,[], () => {
+      const localData = localStorage.getItem('data');
+      return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+      localStorage.setItem('data',JSON.stringify(data))
+    },[data]);
+
+    return (
+      <UserContext.Provider value={{data,setData}}>
+        {props.childern}
+      </UserContext.Provider>
+     )
+  }
  
- export const Component1 = ({childern}) => {
-  const [userDetails, setUserDetails] = useState();
 
-  const value = useContext(
-    () => ({
-      userDetails,setUserDetails,
-    }),
-    [userDetails,setUserDetails]
-  )
-  return (
-    <UserContext.Provider value={value}>
-      {childern}
-    </UserContext.Provider>
-  )
-}
-
-// export const Component2 = () = > {
+// export const Header = () => {
 //   return (
-//     React.useContext(UserContext);
-//   )
-// }
+//     <nav id="navigation">
+//       <h1 href="Login" className="logo">
+//         <Login />
+//       </h1>
+//     </nav>
+//   );
+// };
