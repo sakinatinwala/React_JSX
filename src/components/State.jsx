5660cd991fcd5b9f9fc10ae86451f297f0c10e3f
Sakina Tinwala <tinwalasakina515253@gmail.com>
2022-10-19 16:13:56 +0530

    -React Initial Commited..

--- src/components/State.jsx
+++
@@ -1,9 +0,0 @@
-import {createStore} from "redux";
-
-const counterReducer = (state = {counter:0},action) => {
-    if (action.type === "Data" ) {
-        return {counter:state.data};
-    }
-}
-
-export const store = createStore(counterReducer);
